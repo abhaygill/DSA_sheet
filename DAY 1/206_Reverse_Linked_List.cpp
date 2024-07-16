cdf219dd53bc5d18abb15375d4873c29d72b84ce
Abhay Singh Gill <112624949+abhaygill@users.noreply.github.com>
2024-07-16 19:34:31 +0530

    Create 206_Reverse_Linked_List.cpp

---
+++ DAY 1/206_Reverse_Linked_List.cpp
@@ -0,0 +1,19 @@
+
+//Best Solution.
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* curr= head;
+        ListNode* prev= NULL;
+        while(curr != NULL){
+            ListNode* temp = curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr= temp;
+        }
+        return prev;
+    }
+};
